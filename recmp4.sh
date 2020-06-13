72509553c7ffbf145ab4f03c085c2974f1318745
Isaias Neto <isaiasneto@gmail.com>
2020-06-13 19:53:20 -0300

    Refactored record comand line

--- recmp4.sh
+++ recmp4.sh
@@ -9,23 +9,12 @@ destldir="${8}"
 
 n=1
 while [[ ${n} -eq 1 ]] ; do
-		if [[ "${aspect}" == "null" ]] ; then
-			# FFREPORT=file=${destldir}/${destfile}"_"info.log:level=32 \
-			# FFREPORT=file=${destldir}/${destfile}"_"warning.log:level=24 \
-			FFREPORT=file=${destldir}/${destfile}"_"error.log:level=16 \
-			ffmpeg -loglevel quiet \
-			-f mpeg -i "${sourcec}" \
-			-vf yadif -c:v libx264 -preset superfast \
-			-f segment -strftime 1 -segment_time ${segtime} -reset_timestamps 1 -segment_time_metadata 1 \
-			${destdir}/%Y-%m-%d"_"%H-%M-%S"_"${destfile}.mp4
-		else
-			#FFREPORT=file=${destldir}/${destfile}"_"info.log:level=32 \
-			# FFREPORT=file=${destldir}/${destfile}"_"warning.log:level=24 \
-			FFREPORT=file=${destldir}/${destfile}"_"error.log:level=16 \
-			ffmpeg -loglevel quiet \
-			-f mpeg -i "${sourcec}" \
-			-vf yadif -c:v libx264 -preset superfast -aspect ${aspect} \
-			-f segment -strftime 1 -segment_time ${segtime} -reset_timestamps 1 -segment_time_metadata 1 \
-			${destdir}/%Y-%m-%d"_"%H-%M-%S"_"${destfile}.mp4
-		fi
+	#FFREPORT=file=${destldir}/${destfile}"_"info.log:level=32 \
+	# FFREPORT=file=${destldir}/${destfile}"_"warning.log:level=24 \
+	FFREPORT=file=${destldir}/${destfile}"_"error.log:level=16 \
+	ffmpeg -loglevel quiet \
+	-i "${sourcec}" -c:v copy \
+	$([[ "${aspect}" != "null" ]] && echo "-aspect" "${aspect}") \
+	-f segment -strftime 1 -segment_time ${segtime} -reset_timestamps 1 -segment_time_metadata 1 \
+	${destdir}/%Y-%m-%d"_"%H-%M-%S"_"${destfile}.mp4
 done
