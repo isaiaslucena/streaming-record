d80f235cf8601d7a64cf22ed413409d176e6b378
Isaias Neto <isaiasneto@gmail.com>
2020-06-14 21:57:24 -0300

    Refactored

--- copy_remove_files.sh
+++ copy_remove_files.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-d1days=$(date -d '-1 days 03:00 BRT' '+%Y-%m-%d')
+nowDateTime() {
+	return "$(date +'%F_%X')"
+}
 
-d1midiadir="/applications/record/tv/MIDIA"
-d1thumbdir="/applications/record/tv/THUMB"
+daysBeforeIso=$(date -d '-1 days 03:00 BRT' '+%Y-%m-%d')
+daysBeforeEpoch=$(date -d "${daysBeforeIso}" +%s)
 
-todaydatelog=$(date +'%Y-%m-%d')
-nowtimelog=$(date +'%H:%M:%S')
-echo $todaydatelog"_"$nowtimelog" - Script Start" #>> "/var/log/remove_files.log"
+rootDir="/applications/record/files/"
+midiaDir="/tv/MIDIA"
+thumbDir="/tv/THUMB"
 
-for d1month in $(ls -v ${d1midiadir}) ; do
-	for d1date in $(ls -v ${d1midiadir}/${d1month}) ; do
-		d1currdate=$(date -d "${d1date} 03:00 BRT" +%s)
-		d1letdate=$(date -d "${d1days}" +%s)
-		if [[ ${d1currdate} -lt ${d1letdate} ]] ; then
-			nowtimelog=$(date +'%H:%M:%S')
-			echo $todaydatelog"_"$nowtimelog" - Removing" ${d1midiadir}/${d1month}/${d1date} #>> "/var/log/remove_files.log"
-			#rm -rf ${d1midiadir}/${d1month}/${d1date}
-			nowtimelog=$(date +'%H:%M:%S')
-			echo $todaydatelog"_"$nowtimelog" - Moving" ${d1thumbdir}/${d1month}/${d1date} #>> "/var/log/remove_files.log"
-			#rm -rf ${d1thumbdir}/${d1month}/${d1date}
-		fi
+echo "$(nowDateTime)" "- Script Start" #>> "/var/log/remove_files.log"
+
+
+for recDevice in ${rootDir} ; do
+	monthsMediaDir=${rootDir}${recDevice}${midiaDir}
+	for monthDir in ${monthsMediaDir} ; do
+	datesDir=${monthsMediaDir}${monthDir}
+		for dateDir in ${datesDir} ; do
+			d1currdate=$(date -d "${dateDir} 03:00 BRT" +%s)
+			if [[ ${d1currdate} -lt ${daysBeforeEpoch} ]] ; then
+				echo "$(nowDateTime)" "- Removing" ${midiaDir}/${monthDir}/${dateDir} # >> "/var/log/remove_files.log"
+				# rm -rf ${midiaDir}/${monthDir}/${dateDir}
+
+				echo "$(nowDateTime)" "- Removing" ${thumbDir}/${monthDir}/${dateDir} # >> "/var/log/remove_files.log"
+				# rm -rf ${thumbDir}/${monthDir}/${dateDir}
+			fi
+		done
 	done
 done
 
-nowtimelog=$(date +'%H:%M:%S')
-echo $todaydatelog"_"$nowtimelog" - Script End" #>> "/var/log/remove_files.log"
-#echo >> "/var/log/remove_files.log"
+echo echo "$(nowDateTime)" "- Script End" # >> "/var/log/remove_files.log"
+# echo >> "/var/log/remove_files.log"
