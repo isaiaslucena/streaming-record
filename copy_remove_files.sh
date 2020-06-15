69baa0d7721079fe484155bdd316c24e456ab0c2
Isaias Neto <isaiasneto@gmail.com>
2020-06-14 23:38:20 -0300

    Refactored

--- copy_remove_files.sh
+++ copy_remove_files.sh
@@ -5,25 +5,35 @@ nowDateTime() {
 }
 
 daysBeforeIso=$(date -d '-1 days 03:00 BRT' '+%Y-%m-%d')
-daysBeforeEpoch=$(date -d "${daysBeforeIso}" +%s)
+daysBeforeEpoch=$(date -d "${daysBeforeIso} 03:00 BRT" +%s)
 
-rootDir="/applications/record/files/"
+rootDir="/applications/record/files"
 midiaDir="/tv/MIDIA"
-thumbDir="/tv/THUMB"
 
-echo "$(nowDateTime)" "- Script Start" #>> "/var/log/remove_files.log"
+echo "$(nowDateTime)" "- Script Start" # >> "/var/log/remove_files.log"
 
-for recDevice in ${rootDir} ; do
-	monthsMediaDir=${rootDir}${recDevice}${midiaDir}
-	for monthDir in ${monthsMediaDir} ; do
-	datesDir=${monthsMediaDir}${monthDir}
-		for dateDir in ${datesDir} ; do
-			d1currdate=$(date -d "${dateDir} 03:00 BRT" +%s)
-			if [[ ${d1currdate} -lt ${daysBeforeEpoch} ]] ; then
-				echo "$(nowDateTime)" "- Removing" ${midiaDir}/${monthDir}/${dateDir} # >> "/var/log/remove_files.log"
+for recDeviceDir in $(ls -d ${rootDir}/*) ; do
+	# echo "${recDeviceDir}"
+	monthsDir=${recDeviceDir}${midiaDir}
+	for monthDir in $(ls -d ${monthsDir}/*) ; do
+		# echo "${monthDir}"
+		for dateDir in $(ls "${monthDir}") ; do
+			# echo "Current dayDir" "${dateDir}"
+			dateDirEpoch=$(date -d "${dateDir} 03:00 BRT" +%s)
+			if [[ ${dateDirEpoch} -le ${daysBeforeEpoch} ]] ; then
+				currentMediaDir=${monthDir}/${dateDir}
+				currentThumbDir=${currentMediaDir//MIDIA/THUMB}
+				echo ${currentMediaDir}
+				echo ${currentThumbDir}
+				# echo "${dateDir}" "is less or equal" "${daysBeforeIso}"
+				echo "$(nowDateTime)" "- Copying MEDIA dir " ${currentMediaDir} "to remote server..." # >> "/var/log/remove_files.log"
+				rsync -e "ssh -p 2134" -R "${currentMediaDir}" root@serverclip.ddns.net:/
+				echo "$(nowDateTime)" "- Copying THUMB dir " ${currentThumbDir} "to remote server..." # >> "/var/log/remove_files.log"
+				rsync -e "ssh -p 2134" -R "${currentThumbDir}" root@serverclip.ddns.net:/
+
 				# rm -rf ${midiaDir}/${monthDir}/${dateDir}
 
-				echo "$(nowDateTime)" "- Removing" ${thumbDir}/${monthDir}/${dateDir} # >> "/var/log/remove_files.log"
+				# echo "$(nowDateTime)" "- Removing" ${thumbDir}/${monthDir}/${dateDir} # >> "/var/log/remove_files.log"
 				# rm -rf ${thumbDir}/${monthDir}/${dateDir}
 			fi
 		done
