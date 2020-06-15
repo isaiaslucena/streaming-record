62a6a1b8c773a95cb6c015f4879165af642b73c5
Isaias Neto <isaiasneto@gmail.com>
2020-06-14 23:56:04 -0300

    Updated Path

--- verify_thumbs.sh
+++ verify_thumbs.sh
@@ -3,7 +3,7 @@
 checkr=$(pidof -x generate_thumbs.sh)
 # echo ${checkr}
 if [[ -z ${checkr} ]] ; then
-	/tv/scripts/generate_thumbs.sh
+	/applications/record/scripts/generate_thumbs.sh
 else
 	echo "Already running!"
 fi
